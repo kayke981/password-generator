c934bc7f62ad4ff5b930149a567d3a4b1df87430
kayke981 <Quintanilha9090>
2022-01-03 18:42:53 -0300

    ...

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import random
+import os
 
 def change_yesOrno_to_bool(param):
     data = param.lower()
     # transforming 'yes' to 'true' and 'no' to 'false'
     if data == 'yes' or data == 'y':
-        return 'true'
+        return True
     else:
-        return 'false'
+        return False
 
 def password_generator(leng, symbols, numbers):
     abc = 'abcdefghijklmnopqrstuvwxyz'
-    if symbols.lower() == 'true':
-        abc = abc + '@!#$%¨&*()_+=§-\|,.<>;:~^´`[{]}'
-    if numbers.lower() == 'true':
+    if symbols:
+        abc = abc + '@!#$%&*_+=\|[{]}'
+    if numbers:
         abc = abc + '1234567890'
     password_random = [random.choice(abc) for i in range(int(leng))]
     password = ''.join(password_random)
     return password
 
 def main():
+    # questions
     leng = input("How length of password?: ")
     symbols = input("Do you want symbols? [Y/N]: ")
     numbers = input("Do you want numbers? [Y/N]: ")
@@ -30,10 +32,16 @@ def main():
     print(f'[+] Password generated: {password}')
     file = input(f'Do you want write the password ({password}) in a file? [Y/N]: ')
     fileBool = change_yesOrno_to_bool(file)
-    if fileBool == 'true':
-        print('[+] Writing to password.txt...')
-        with open('password.txt', 'a') as file:
+    # writing in a file
+    if fileBool:
+        print('[+] Writing...')
+        with open('util/password.txt', 'a') as file:
             file.write(f'{password}\n')
-            print('[+] Wrote')
+        path = os.path.dirname(os.path.abspath(__file__))
+        if "\\" in path:
+            path = path + "\\util\password.txt"
+        else:
+            path = path + "/util/password.txt"
+        print(f'[+] Wrote to {path}')
     print('[+] Done')
 main()
